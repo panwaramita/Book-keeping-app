69dc3c1a18829a2e61a4acd2dbbc49d99d917e85
Amita <panwar.amita@gmail.com>
2021-11-15 06:01:47 -0500

    adding the route for user registeration

--- backend/route/User.js
+++ backend/route/User.js
@@ -1,2 +1,39 @@
-const express=require(express);
+const express=require('express');
 const usersRoute=express.Router();
+const User=require("../models/User.js");
+
+//user Routes
+//Register
+usersRoute.post('/register',async(req,res)=>{
+    try
+    {
+      const {name,email,password}=req.body;
+      const user= await User.create({name,email,password});    
+      res.send(user);
+  }
+    catch(err)
+    {
+      console.log(err);
+    }
+  })
+  //login
+  usersRoute.post('/login',(req,res)=>{
+      res.send('Login Route')
+  })
+  //update user
+  usersRoute.put('/update',(req,res)=>{
+      res.send('Update Route')
+  })
+  
+  //delete
+  usersRoute.delete('/:id',(req,res)=>{
+      res.send('Delete Route')
+  })
+  
+  //fetch users
+  usersRoute.get('/',(req,res)=>{
+      res.send('Fetch Users')
+  })
+  
+  module.exports=usersRoute;
+  
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const dbconnect=require("./config/dbconnect.js");
-const User=require("./models/User.js");
+const userRoute=require('./route/User.js');
 //connect DB
 dbconnect();
 
@@ -9,41 +9,8 @@ dbconnect();
 
 app.use(express.json());
 //Routes
-//user Routes
-//Register
-app.post('/api/users/register',async(req,res)=>{
-  try
-  {
-    const {name,email,password}=req.body;
-    const user= await User.create({name,email,password});    
-    res.send(user);
-}
-  catch(err)
-  {
-    console.log(err);
-  }
-})
-//login
-app.post('/api/users/login',(req,res)=>{
-    res.send('Login Route')
-})
-//update user
-app.put('/api/users/update',(req,res)=>{
-    res.send('Update Route')
-})
 
-//delete
-app.delete('/api/users/:id',(req,res)=>{
-    res.send('Delete Route')
-})
-
-//fetch users
-app.get('/api/users',(req,res)=>{
-    res.send('Fetch Users')
-})
-
-
-
+app.use('/api/users',userRoute)
 //server
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
